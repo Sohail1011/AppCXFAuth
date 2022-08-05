58d6ce03f6b1c5c439d763467b1fa0bfd5f45e6a
Sohail1011 <aratavalga@gmail.com>
2022-08-04 22:55:08 -0500

    trabajo

--- src/main/java/pe/company/ws/ClienteWS.java
+++ src/main/java/pe/company/ws/ClienteWS.java
@@ -5,9 +5,13 @@ import java.util.stream.Collectors;
 
 import javax.jws.WebService;
 
+import org.apache.cxf.interceptor.InInterceptors;
+
 import pe.company.dao.ClienteDAO;
 import pe.company.model.Cliente;
+import pe.company.ws.security.WSSecurityInterceptor;
 
+@InInterceptors(classes = WSSecurityInterceptor.class)
 @WebService(targetNamespace = "http://ws.company.pe/", endpointInterface = "pe.company.ws.ClienteWSInterface", portName = "ClienteWSPort", serviceName = "ClienteWSService")
 public class ClienteWS implements ClienteWSInterface {
 
